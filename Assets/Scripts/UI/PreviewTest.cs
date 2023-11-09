9467194ac8b916364fdcc48b688d69ef589bbf3e
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-11-09 22:46:19 +0900

    [add]UIのテストをできるシーンを追加 [clean]ファイル整理

---
+++ Assets/Scripts/UI/PreviewTest.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PreviewTest : MonoBehaviour
+{
+    [SerializeField] Image _image;
+    [SerializeField] Sprite[] _sprite;
+}
