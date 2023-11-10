c9b0f5532e7c4df85cea841b92bb7888d8d9b98e
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-11-10 23:54:25 +0900

    [add]マウスドラッグでのオブジェクトの回転機能の追加

--- Assets/Scripts/UI/PreviewTest.cs
+++ Assets/Scripts/UI/PreviewTest.cs
@@ -6,4 +6,54 @@ public class PreviewTest : MonoBehaviour
 {
     [SerializeField] Image _image;
     [SerializeField] Sprite[] _sprite;
+    [SerializeField] GameObject targetObject;
+    [SerializeField] Vector2 rotationSpeed = new Vector2(0.1f, 0.2f);
+    [SerializeField] bool reverse;
+
+    Camera mainCamera;
+    Vector2 lastMousePosition;
+
+    void Start()
+    {
+        //mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            if (!reverse)
+            {
+                var x = (Input.mousePosition.y - lastMousePosition.y);
+                var y = (lastMousePosition.x - Input.mousePosition.x);
+
+                var newAngle = Vector3.zero;
+                newAngle.x = x * rotationSpeed.x;
+                newAngle.y = y * rotationSpeed.y;
+
+                targetObject.transform.Rotate(newAngle);
+                lastMousePosition = Input.mousePosition;
+
+                Debug.Log($"XMove : {x}\nYMove : {y}");
+            }
+            else
+            {
+                var x = (lastMousePosition.y - Input.mousePosition.y);
+                var y = (Input.mousePosition.x - lastMousePosition.x);
+
+                var newAngle = Vector3.zero;
+                newAngle.x = x * rotationSpeed.x;
+                newAngle.y = y * rotationSpeed.y;
+
+                targetObject.transform.Rotate(newAngle);
+                lastMousePosition = Input.mousePosition;
+
+                Debug.Log($"XMove : {x}\nYMove : {y}");
+            }
+        }
+    }
 }
