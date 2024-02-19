c161548ab516e4a440131bc88b403785b541288d
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2024-02-19 23:57:15 +0900

    [add]集中状態の試作の開始

---
+++ Assets/Scripts/GameManager/IZoneTime.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>集中状態の処理を行うためのインターフェース</summary>
+public interface IZoneTime
+{
+    /// <summary>集中状態に入った時の処理</summary>
+    public void OnEnterZoneTime();
+
+    /// <summary>集中状態中の処理</summary>
+    public void OnUpdateZoneTime();
+
+    /// <summary>集中状態ｗｐ終了するときの処理</summary>
+    public void OnExitZoneTime();
+}
